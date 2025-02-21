bd692b55809609eb06322eb13436f46fd9bb4ed1
Joey Blackwell <blackwellj6241@gmail.com>
2025-02-21 09:41:49 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+export const App = () => {
+  return( 
+  <div className="app-container">
+    <div className="app-heading">
+      <h1 className="app-heading-text">Chuckle Checklist</h1> 
+    </div>       
+  </div>
+  
+  )
+}
