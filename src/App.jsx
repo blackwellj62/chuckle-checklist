34faf1dfb3098ad4fcad3f53bfb3f6e234bd0444
Joey Blackwell <blackwellj6241@gmail.com>
2025-02-24 16:08:02 -0600

    added told,untold, and delete buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,74 @@
 import { useEffect, useState } from "react";
-import { postNewJoke } from "./services/jokeService.jsx";
+import { deleteJoke, editJoke, postNewJoke } from "./services/jokeService.jsx";
 import { getAllJokes } from "./services/jokeService.jsx";
-import stevePic from "./assets/steve.png"
+import stevePic from "./assets/steve.png";
 import "./App.css";
 
 export const App = () => {
-  const [userInput, setUserInput] = useState('')
-  const [allJokes, setAllJokes] = useState([])
-  const [untoldJokes, setUntoldJokes] = useState('')
-  const [toldJokes, setToldJokes] = useState([])
-   
-useEffect(()=>{
-    getAllJokes().then((jokesArray)=>{
-      setAllJokes(jokesArray)
-      console.log("jokes set!")
-    })
-}, [])
+  const [userInput, setUserInput] = useState("");
+  const [allJokes, setAllJokes] = useState([]);
+  const [untoldJokes, setUntoldJokes] = useState([]);
+  const [toldJokes, setToldJokes] = useState([]);
 
-// useEffect(()=>{
-//   if (untoldJokes === false){
-//     const untold = allJokes.filter(joke => joke.told === false)
-//     setUntoldJokes(untold)
-//   }
-// }, [untoldJokes, allJokes])
+  useEffect(() => {
+    getAllJokes().then((jokesArray) => {
+      setAllJokes(jokesArray);
+      console.log("jokes set!");
+    });
+  }, []);
+
+  useEffect(() => {
+    const untold = allJokes.filter((joke) => joke.told === false);
+    setUntoldJokes(untold);
+
+    const told = allJokes.filter((joke) => joke.told === true);
+    setToldJokes(told);
+  }, [allJokes]);
 
   const handleJokeChange = (event) => {
-      setUserInput(event.target.value)
+    setUserInput(event.target.value);
+  };
+  const handleSubmit = async () => {
+    await postNewJoke(userInput);
+    setUserInput("");
+    reFetch()
+  };
+
+  const reFetch = () => {
+    getAllJokes().then((jokesArray) => {
+      setAllJokes(jokesArray);
+    })
   }
-const handleSubmit = async () => {
-  await postNewJoke(userInput)
-  return setUserInput('')
+  
+const handleEditJoke = async (joke) => {
+  let changedJoke = {
+    text: joke.text,
+    told: !joke.told
+  }
+   
+   await editJoke(changedJoke, joke.id)
+    reFetch()
+
+}
+
+const handleDeletedJoke = async (joke) => {
+  let deletedJoke = {
+    text: joke.text,
+    told: joke.told
+  }
+  await deleteJoke(deletedJoke, joke.id)
+  reFetch()
 }
 
   return (
     <div className="app-container">
       <div className="app-heading">
-      <div className="app-heading-circle">
-   <img className="app-logo" src={stevePic} alt="Good job Steve" />
- </div>
+        <div className="app-heading-circle">
+          <img className="app-logo" src={stevePic} alt="Good job Steve" />
+        </div>
         <h1 className="app-heading-text">Chuckle Checklist</h1>
       </div>
-        <h2 className="h2"> Add Joke </h2>
+      <h2 className="h2"> Add Joke </h2>
       <div className="joke-add-form">
         <input
           className="joke-input"
@@ -49,22 +77,41 @@ const handleSubmit = async () => {
           placeholder="New One Liner"
           onChange={handleJokeChange}
         />
-        <button className="joke-input-submit" onClick={handleSubmit}>Add</button>
+        <button className="joke-input-submit" onClick={handleSubmit}>
+          Add
+        </button>
       </div>
       <div className="joke-lists-container">
         <div className="joke-list-container">
-          <h2 className="h2">Untold</h2>
-          {/* <ul>
-            <li joke-list-item>{untoldJokes}</li>
-          </ul> */}
-          </div>
+          <h2 className="h2">Untold 
+            <span className="untold-count">{untoldJokes.length}</span>
+          </h2>
+          <ul>
+            {untoldJokes.map((joke) => (
+              <li key={joke.id} className="joke-list-item">
+                {joke.text}
+              <button onClick={()=>{handleEditJoke(joke)}}>✔️</button>
+              <button onClick={()=>{handleDeletedJoke(joke)}}>🗑️</button>
+              </li>
+              
+            ))}
+          </ul>
+        </div>
         <div className="joke-list-container">
-          <h2 className="h2">Told</h2>
-          </div>
-
+          <h2 className="h2">Told
+            <span className="told-count">{toldJokes.length}</span>
+          </h2>
+          <ul>
+            {toldJokes.map((joke) => (
+              <li key={joke.id} className="joke-list-item">
+                {joke.text}
+                <button onClick={()=>{handleEditJoke(joke)}}>✖️</button>
+                <button onClick={()=>{handleDeletedJoke(joke)}}>🗑️</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-        
-      
     </div>
   );
 };
--- src/services/jokeService.jsx
+++ src/services/jokeService.jsx
@@ -1,20 +1,48 @@
+
+
 export const getAllJokes = async () => {
     const response = await fetch('http://localhost:8088/jokes')
     const jokes = await response.json()
     return jokes
 }
 
-export const postNewJoke = async (state) => {
+export const postNewJoke = async (text) => {
     const postOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            state,
+            text,
             told: false
         })
     }
     const response = await fetch('http://localhost:8088/jokes', postOptions)
 
+}
+
+export const editJoke = async (changedJoke, id) => {
+    const editOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            changedJoke
+        )
+    }
+    await fetch(`http://localhost:8088/jokes/${id}`, editOptions)
+}
+
+export const deleteJoke = async (deletedJoke, id) => {
+    const deleteOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            deletedJoke
+        )
+    }
+    await fetch(`http://localhost:8088/jokes/${id}`, deleteOptions)  
 }
